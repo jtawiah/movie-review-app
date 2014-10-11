2b37f81f8e7e51a469f6134b02151dc7ec485f4c
John Akuoko-Tawiah <boatjohn87@gmail.com>
2014-10-11 09:22:25 -0600

    start movie review app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moviereview::Application.config.secret_key_base = 'c15d7997e2607c3a278fe95e507539fdefe146cba8f0e70abd4c7f7f8818bb29045d87d9bd318028916c7dd58a285dbeb4f5e3e4ceb93528974f0c1fa37fd3a4'
