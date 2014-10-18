7001b597d8ad475f870ea14606894fc89ef84e29
jtawiah <boatjohn87@gmail.com>
2014-10-18 13:22:06 +0000

    Install Bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+	def flash_message(type)
+		type == :notice ? "alert-success" : "alert-danger"
+
+		# case type
+		# when :notice
+		#  "alert-success"
+		# when :alert
+		#  "alert-danger"
+		# else
+		#   ""
+		# end
+	end
 end
